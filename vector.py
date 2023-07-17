f88ca105c084d27987933687630459e1cb16f1fe
Siddhartha Purwar <siddpurwar@gmail.com>
2023-07-17 16:22:29 +0530

    FIX: using decimal for more precision

--- vector.py
+++ vector.py
@@ -1,32 +1,47 @@
 from typing import Union
+from math import sqrt, pi, acos
+from decimal import Decimal, getcontext
 
-int_float = Union[int, float]
+getcontext().prec = 30
+
+int_float_decimal = Union[int, float, Decimal]
 
 class Vector(object):
     def __init__(self, coordinates) -> None:
         try:
             if not coordinates:
                 raise ValueError
-            if any(type(coordinate) != int_float for coordinate in coordinates) == False:
+            if any(type(coordinate) != int_float_decimal for coordinate in coordinates) == False:
                 raise TypeError
-            self.coordinates = tuple(coordinates)
-            self.dimension = len(coordinates)
-            self.magnitute = pow(sum([x*x for x in self.coordinates]), .5)
+            self.coordinates = tuple(Decimal(coordinate) for coordinate in coordinates)
+            self.dimensions = len(coordinates)
+            self.magnitute = Decimal(sqrt(sum([x*x for x in self.coordinates])))
         
         except ValueError:
             raise ValueError('Coordinates cannot be empty')
         except TypeError:
             raise TypeError('Coordinates should be iterable and all values should be int/float')
     
-   
-    def get_direction(self) -> int_float:
+    def get_magnitute(self) -> Decimal:
+        return Decimal(sqrt(sum([x*x for x in self.coordinates])))
+    
+    def get_unit_vector(self) -> Decimal:
         try:
-            if self.magnitute == 0 or self.magnitute == 0.0:
+            if self.magnitute == Decimal('0.0'):
                 raise ZeroDivisionError
-            return self * (1 / self.magnitute)
+            return self * (Decimal('1.0') / self.magnitute)
+        
         except ZeroDivisionError:
-            raise("Magnitute is zero, it is a zero vector.")
-
+            raise ZeroDivisionError(f"Magnitute is zero, {self} is a zero vector.")
+    
+    def get_angle_with(self, v: object, in_degrees=False) -> Decimal:
+        dot_product = self * v
+        angle_in_radian = acos(dot_product / (self.get_magnitute() * v.get_magnitute()))
+        if in_degrees:
+            degree_per_radian = 180. / pi
+            angle_in_degree = Decimal(angle_in_radian * degree_per_radian)
+            return angle_in_degree
+        return angle_in_radian
     
     def __add__(self, v: object) -> object:
         new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
@@ -37,17 +52,25 @@ class Vector(object):
         return Vector(new_coordinates)
 
     def __mul__(self, v: object) -> object:
-        if isinstance(v, (int, float)):
+        if isinstance(v, (int, float, Decimal)):
+            v = Decimal(v)
             try:
-                if v == 0 or v == 0.0:
+                if v == Decimal('0.0'):
                     raise ZeroDivisionError
-                new_coordinates = [x * v for x in self.coordinates]
+                new_coordinates = [Decimal(x * v) for x in self.coordinates]
                 return Vector(new_coordinates)
             except ZeroDivisionError:
                 raise ZeroDivisionError('Coordinates cannot be empty')
+            
         elif isinstance(v, object):
-                new_coordinates = [x * y for x, y in zip(self.coordinates, v.coordinates)]
-                return Vector(new_coordinates)
+                try:
+                    if v.dimensions != self.dimensions:
+                        raise ValueError
+                    dot_product = sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
+                    return dot_product
+                except ValueError:
+                    raise ValueError('Dimensions should be equal')
+    
         
     def __str__(self) -> str:
         return f'Vector: {self.coordinates}'
@@ -56,25 +79,19 @@ class Vector(object):
         return self.coordinates == value.coordinates
 
 
-v1 = Vector([1, 2, 3])
-print(v1)
+# v1 = Vector([1, 2, 3])
+# print(v1)
 
-v2 = Vector([3, 4, 5])
-v3 = Vector([1, 2, 3])
-print(v1 == v3)
+v2 = Vector([2.571, 8.259, 3.985])
+v3 = Vector([2.571, 8.259, 3.985])
+# print(v2 == v3)
 
-v4 = v2 + v3
-print(v4)
+# print(f"v2 + v3 is equal to {v2 + v3}")
+# print(f"v2 - v3 is equal to {v2 - v3}")
+# print(f"v2 * 10 is equal to {v2 * 10 }")
+# print(f"Dot product of two v2 and v3 is equal to {v2 * v3}")
 
-v5 = v2 - v3
-print(v5)
+print(f"||v2|| equals to {v2.get_magnitute()}")
+print(f"Unit vector of v2 equals to {v2.get_unit_vector()}")
 
-v6 = v2 * 10 
-print(v6)
-
-v7 = v2 * v3 
-print(v7)
-
-print(v7.magnitute)
-v7_unit = v7.get_direction()
-print(v7_unit)
+print(f"Angle between two v2 and v3 is equal to {v2.get_angle_with(v3, in_degrees=True)}")
