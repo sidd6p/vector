ffcba233f66e44b0324db457b6f64eca977b15f1
Siddhartha Purwar <siddpurwar@gmail.com>
2023-07-17 15:20:08 +0530

    BUILT: magnitute and direction

--- vector.py
+++ vector.py
@@ -1,17 +1,32 @@
+from typing import Union
+
+int_float = Union[int, float]
+
 class Vector(object):
     def __init__(self, coordinates) -> None:
         try:
             if not coordinates:
                 raise ValueError
-            if any(type(coordinate) != int for coordinate in coordinates):
+            if any(type(coordinate) != int_float for coordinate in coordinates) == False:
                 raise TypeError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
+            self.magnitute = pow(sum([x*x for x in self.coordinates]), .5)
         
         except ValueError:
             raise ValueError('Coordinates cannot be empty')
         except TypeError:
             raise TypeError('Coordinates should be iterable and all values should be int/float')
+    
+   
+    def get_direction(self) -> int_float:
+        try:
+            if self.magnitute == 0 or self.magnitute == 0.0:
+                raise ZeroDivisionError
+            return self * (1 / self.magnitute)
+        except ZeroDivisionError:
+            raise("Magnitute is zero, it is a zero vector.")
+
     
     def __add__(self, v: object) -> object:
         new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
@@ -23,11 +38,16 @@ class Vector(object):
 
     def __mul__(self, v: object) -> object:
         if isinstance(v, (int, float)):
-            new_coordinates = [x * v for x in self.coordinates]
-            return Vector(new_coordinates)
+            try:
+                if v == 0 or v == 0.0:
+                    raise ZeroDivisionError
+                new_coordinates = [x * v for x in self.coordinates]
+                return Vector(new_coordinates)
+            except ZeroDivisionError:
+                raise ZeroDivisionError('Coordinates cannot be empty')
         elif isinstance(v, object):
-            new_coordinates = [x * y for x, y in zip(self.coordinates, v.coordinates)]
-            return Vector(new_coordinates)
+                new_coordinates = [x * y for x, y in zip(self.coordinates, v.coordinates)]
+                return Vector(new_coordinates)
         
     def __str__(self) -> str:
         return f'Vector: {self.coordinates}'
@@ -54,3 +74,7 @@ print(v6)
 
 v7 = v2 * v3 
 print(v7)
+
+print(v7.magnitute)
+v7_unit = v7.get_direction()
+print(v7_unit)
