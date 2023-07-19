d4713242983fd9cfe2f36f0e08ddcf7c7cd2f704
Siddhartha Purwar <siddpurwar@gmail.com>
2023-07-19 16:26:11 +0530

    REFACTORED: dunder function are at top now

--- vector.py
+++ vector.py
@@ -28,7 +28,7 @@ class Vector(object):
                 raise TypeError
             self.coordinates = tuple(Decimal(coordinate) for coordinate in coordinates)
             self.dimensions = len(coordinates)
-            self.magnitute = Decimal(sqrt(sum([x * x for x in self.coordinates])))
+            self.magnitude = Decimal(sqrt(sum([x * x for x in self.coordinates])))
 
         except ValueError:
             raise ValueError("Coordinates cannot be empty")
@@ -37,6 +37,92 @@ class Vector(object):
                 "Coordinates should be iterable and all values should be int/float"
             )
 
+    def __add__(self, v: object) -> object:
+        """
+        Adds two vectors.
+
+        :param v: The vector to add.
+        :return: The result of the vector addition.
+        """
+        new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def __sub__(self, v: object) -> object:
+        """
+        Subtracts one vector from another.
+
+        :param v: The vector to subtract.
+        :return: The result of the vector subtraction.
+        """
+        new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def __mul__(self, v: object) -> object:
+        """
+        Multiplies the vector by a scalar or calculates the dot product with another vector.
+
+        :param v: The scalar value or the other vector.
+        :return: The result of the multiplication.
+        """
+        if isinstance(v, (int, float, Decimal)):
+            v = Decimal(v)
+            new_coordinates = [Decimal(x * v) for x in self.coordinates]
+            return Vector(new_coordinates)
+
+        elif isinstance(v, object):
+            try:
+                if v.dimensions != self.dimensions:
+                    raise ValueError
+                dot_product = sum(
+                    [x * y for x, y in zip(self.coordinates, v.coordinates)]
+                )
+                return dot_product
+            except ValueError:
+                raise ValueError("Dimensions should be equal")
+
+    def __truediv__(self, v: object) -> object:
+        if isinstance(v, (int, float, Decimal)):
+            v = Decimal(v)
+            try:
+                if Decimal(v) == Decimal("0.0"):
+                    raise ZeroDivisionError
+                new_coordinates = [Decimal(x / v) for x in self.coordinates]
+                return Vector(new_coordinates)
+            except ZeroDivisionError:
+                raise ZeroDivisionError("Value cannot be zero")
+
+        elif isinstance(v, object):
+            try:
+                if v.dimensions != self.dimensions:
+                    raise ValueError
+                new_coordinates = list()
+                for x, y in zip(self.coordinates, v.coordinates):
+                    if Decimal(y) == Decimal(0.0):
+                        raise ZeroDivisionError
+                    new_coordinates.append(x / y)
+                return Vector(new_coordinates)
+            except ValueError:
+                raise ValueError("Dimensions should be equal")
+            except ZeroDivisionError:
+                raise ZeroDivisionError(f"{v} has zero coordinate/s")
+
+    def __str__(self) -> str:
+        """
+        Returns the string representation of the vector.
+
+        :return: The string representation of the vector.
+        """
+        return f"Vector: {self.coordinates}"
+
+    def __eq__(self, value: object) -> bool:
+        """
+        Checks if two vectors are equal.
+
+        :param value: The other vector to compare.
+        :return: True if the vectors are equal, False otherwise.
+        """
+        return self.coordinates == value.coordinates
+
     def is_zero(self, tolerance: float = 1e-10):
         """
         Checks if the vector is a zero vector.
@@ -44,7 +130,7 @@ class Vector(object):
         :param tolerance: Tolerance for considering a vector as zero (default: 1e-10).
         :return: True if the vector is a zero vector, False otherwise.
         """
-        return self.magnitute < tolerance
+        return self.magnitude < tolerance
 
     def get_unit_vector(self) -> Decimal:
         """
@@ -55,10 +141,10 @@ class Vector(object):
         try:
             if self.is_zero():
                 raise ZeroDivisionError
-            return self * (Decimal("1.0") / self.magnitute)
+            return self * (Decimal("1.0") / self.magnitude)
 
         except ZeroDivisionError:
-            raise ZeroDivisionError(f"Magnitute is zero, {self} is a zero vector.")
+            raise ZeroDivisionError(f"magnitude is zero, {self} is a zero vector.")
 
     def get_angle_with(self, v: object, in_degrees=False) -> float:
         """
@@ -71,7 +157,7 @@ class Vector(object):
         if self.is_zero() or v.is_zero():
             return 0
         dot_product = self * v
-        angle_in_radian = acos(dot_product / (self.magnitute * v.magnitute))
+        angle_in_radian = acos(dot_product / (self.magnitude * v.magnitude))
         if in_degrees:
             degree_per_radian = 180.0 / pi
             angle_in_degree = angle_in_radian * degree_per_radian
@@ -107,11 +193,11 @@ class Vector(object):
         try:
             if v.is_zero():
                 raise ZeroDivisionError
-            return v.get_unit_vector() * (self * v / v.magnitute)
+            return v.get_unit_vector() * (self * v / v.magnitude)
         except ZeroDivisionError:
             raise ZeroDivisionError(f"{v} is zero-vector")
 
-    def get_orthognal_to(self, v: object) -> object:
+    def get_orthogonal_to(self, v: object) -> object:
         """
         Returns the component of the vector orthogonal to another vector.
 
@@ -129,133 +215,77 @@ class Vector(object):
         """
         pass
 
-    def __add__(self, v: object) -> object:
-        """
-        Adds two vectors.
 
-        :param v: The vector to add.
-        :return: The result of the vector addition.
-        """
-        new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
+# Create two Vector objects v1 and v2
+v1 = Vector([1, 2, 0])
+v2 = Vector([1, 2, 9])
 
-    def __sub__(self, v: object) -> object:
-        """
-        Subtracts one vector from another.
+# Print the vectors v1 and v2
+print("v1:", v1)
+print("v2:", v2)
 
-        :param v: The vector to subtract.
-        :return: The result of the vector subtraction.
-        """
-        new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
-
-    def __mul__(self, v: object) -> object:
-        """
-        Multiplies the vector by a scalar or calculates the dot product with another vector.
-
-        :param v: The scalar value or the other vector.
-        :return: The result of the multiplication.
-        """
-        if isinstance(v, (int, float, Decimal)):
-            v = Decimal(v)
-            try:
-                if v.is_zero():
-                    raise ZeroDivisionError
-                new_coordinates = [Decimal(x * v) for x in self.coordinates]
-                return Vector(new_coordinates)
-            except ZeroDivisionError:
-                raise ZeroDivisionError("Coordinates cannot be empty")
-
-        elif isinstance(v, object):
-            try:
-                if v.dimensions != self.dimensions:
-                    raise ValueError
-                dot_product = sum(
-                    [x * y for x, y in zip(self.coordinates, v.coordinates)]
-                )
-                return dot_product
-            except ValueError:
-                raise ValueError("Dimensions should be equal")
-
-    def __str__(self) -> str:
-        """
-        Returns the string representation of the vector.
-
-        :return: The string representation of the vector.
-        """
-        return f"Vector: {self.coordinates}"
-
-    def __eq__(self, value: object) -> bool:
-        """
-        Checks if two vectors are equal.
-
-        :param value: The other vector to compare.
-        :return: True if the vectors are equal, False otherwise.
-        """
-        return self.coordinates == value.coordinates
-
-
-# Create two Vector objects v2 and v3
-v2 = Vector([1, 2, 3])
-v3 = Vector([1, 2, 9])
-
-# Check if v2 is equal to v3 and print the result
-print(f"Are v2 and v3 equal? {v2 == v3}")
+# Check if v1 is equal to v2 and print the result
+are_v1_and_v2_equal = v1 == v2
+print(f"Are v1 and v2 equal? {are_v1_and_v2_equal}")
 
 # Perform vector addition, subtraction, scalar multiplication, and dot product
-v2_plus_v3 = v2 + v3
-v2_minus_v3 = v2 - v3
-v2_times_10 = v2 * 10
-dot_product_v2_v3 = v2 * v3
+v1_plus_v2 = v1 + v2
+v1_minus_v2 = v1 - v2
+v1_times_10 = v1 * 10
+dot_product_v1_v2 = v1 * v2
+v1_by_10 = v1 / 10
+v1_divides_by_v2 = v1 / v2
 
 # Print the results of the operations
-print(f"v2 + v3 is equal to {v2_plus_v3}")
-print(f"v2 - v3 is equal to {v2_minus_v3}")
-print(f"v2 * 10 is equal to {v2_times_10}")
-print(f"Dot product of v2 and v3 is equal to {dot_product_v2_v3}")
+print(f"v1 + v2 is equal to {v1_plus_v2}")
+print(f"v1 - v2 is equal to {v1_minus_v2}")
+print(f"v1 * 10 is equal to {v1_times_10}")
+print(f"Dot product of v1 and v2 is equal to {dot_product_v1_v2}")
+print(f"v1 / 10 is equal to {v1_by_10}")
+print(f"v1 divides by v2 is equal to {v1_divides_by_v2}")
 
-# Calculate the magnitude of v2 and print the result
-magnitude_v2 = v2.magnitute
-print(f"The magnitude of v2 is {magnitude_v2}")
+# Calculate the magnitude of v1 and print the result
+magnitude_v1 = v1.magnitude
+print(f"The magnitude of v1 is {magnitude_v1}")
 
-# Calculate the unit vector of v2 and print the result
-unit_vector_v2 = v2.get_unit_vector()
-print(f"The unit vector of v2 is {unit_vector_v2}")
+# Calculate the unit vector of v1 and print the result
+unit_vector_v1 = v1.get_unit_vector()
+print(f"The unit vector of v1 is {unit_vector_v1}")
 
-# Calculate the angle between v2 and v3 (in radians) and print the result
-angle_v2_v3_radians = v2.get_angle_with(v3, in_degrees=False)
-print(f"Angle between v2 and v3 (in radians) is equal to {angle_v2_v3_radians}")
+# Calculate the angle between v1 and v2 (in radians) and print the result
+angle_v1_v2_radians = v1.get_angle_with(v2, in_degrees=False)
+print(f"Angle between v1 and v2 (in radians) is equal to {angle_v1_v2_radians}")
 
-# Check if v2 is orthogonal and parallel to v3 and print the results
-is_orthogonal_v2_v3 = v2.is_orthogonal_to(v3)
-is_parallel_v2_v3 = v2.is_parallel_to(v3)
-print(f"Is v2 orthogonal to v3? {is_orthogonal_v2_v3}")
-print(f"Is v2 parallel to v3? {is_parallel_v2_v3}")
+# Check if v1 is orthogonal and parallel to v2 and print the results
+is_orthogonal_v1_v2 = v1.is_orthogonal_to(v2)
+is_parallel_v1_v2 = v1.is_parallel_to(v2)
+print(f"Is v1 orthogonal to v2? {is_orthogonal_v1_v2}")
+print(f"Is v1 parallel to v2? {is_parallel_v1_v2}")
 
-# Calculate the component of v2 parallel and orthogonal to v3 and print the results
-v2_parallel_to_v3 = v2.get_parallel_to(v3)
-v2_orthogonal_to_v3 = v2.get_orthognal_to(v3)
-print(f"V2's component parallel to v3 is {v2_parallel_to_v3}")
-print(f"V2's component orthogonal to v3 is {v2_orthogonal_to_v3}")
+# Calculate the component of v1 parallel and orthogonal to v2 and print the results
+v1_parallel_to_v2 = v1.get_parallel_to(v2)
+v1_orthogonal_to_v2 = v1.get_orthogonal_to(v2)
+print(f"V1's component parallel to v2 is {v1_parallel_to_v2}")
+print(f"V1's component orthogonal to v2 is {v1_orthogonal_to_v2}")
 
-# Check if v2 can be represented as the sum of its components parallel and orthogonal to v3 and print the result
-is_v2_parallel_orthogonal_to_v3 = v2 == v2_parallel_to_v3 + v2_orthogonal_to_v3
+# Check if v1 can be represented as the sum of its components parallel and orthogonal to v2 and print the result
+is_v1_parallel_orthogonal_to_v2 = v1 == v1_parallel_to_v2 + v1_orthogonal_to_v2
 print(
-    f"V2 = V2's component parallel to v3 + V2's component orthogonal to v3? {is_v2_parallel_orthogonal_to_v3}"
+    f"V1 = V1's component parallel to v2 + V1's component orthogonal to v2? {is_v1_parallel_orthogonal_to_v2}"
 )
 
-# Calculate the angle between v3 and its component parallel to v2 (in degrees) and print the result
-angle_v3_with_parallel_to_v2 = v3.get_angle_with(
-    v2.get_parallel_to(v3), in_degrees=True
-)
-print(
-    f"Angle between v3 and its component parallel to v2 (in degrees) is {angle_v3_with_parallel_to_v2}"
-)
-
-# Calculate the angle between v3 and its component orthogonal to v2 (in degrees) and print the result
-angle_v3_with_orthogonal_to_v2 = v3.get_angle_with(
-    v2.get_orthognal_to(v3), in_degrees=True
+# Calculate the angle between v2 and its component parallel to v1 (in degrees) and print the result
+angle_v2_with_parallel_to_v1 = v2.get_angle_with(
+    v1.get_parallel_to(v2), in_degrees=True
 )
 print(
-    f"Angle between v3 and its component orthogonal to v2 (in degrees) is {angle_v3_with_orthogonal_to_v2}"
+    f"Angle between v2 and its component parallel to v1 (in degrees) is {angle_v2_with_parallel_to_v1}"
+)
+
+# Calculate the angle between v2 and its component orthogonal to v1 (in degrees) and print the result
+angle_v2_with_orthogonal_to_v1 = v2.get_angle_with(
+    v1.get_orthogonal_to(v2), in_degrees=True
+)
+print(
+    f"Angle between v2 and its component orthogonal to v1 (in degrees) is {angle_v2_with_orthogonal_to_v1}"
 )
